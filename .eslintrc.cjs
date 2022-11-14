6d352c4e34d1185b3a43b4d2113b27ee56bc9479
Felix Bastian <f.bastian@campus.lmu.de>
2022-11-14 13:11:19 +0100

    Setup Vite + Svelte with example (D3) bar chart

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/* eslint-env node */
+
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:svelte/recommended",
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module",
+	},
+	"rules": {
+		"indent": ["error", "tab"],
+		"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+		"no-trailing-spaces": "error",
+		"eol-last": ["error", "always"],
+		"linebreak-style": ["error", "unix"],
+
+		"comma-dangle": ["warn", "always-multiline"],
+		"quotes": ["warn", "double"],
+		"semi": ["warn", "always"],
+		"eqeqeq": ["error", "always"],
+		"no-var": "error",
+	},
+	"overrides": [
+		{
+			"files": ["*.svelte"],
+			"rules": {
+				"indent": "off",
+				"svelte/indent": ["error", { "indent": "tab" }],
+				"no-self-assign": "off",
+			},
+		},
+	],
+};
---
+++ src/lib/BarCharts.svelte
@@ -0,0 +1,46 @@
+<script>
+	import BarChartReactive from "./BarChartReactive.svelte";
+	import BarChartD3 from "./BarChartD3.svelte";
+
+	let values = [40, 86, 168, 281, 303, 365, 400];
+
+	function randomize() {
+		values = values.map((value) => Math.round(value + (Math.random() * 20 - 10)));
+	}
+
+	function add() {
+		values = [...values, (values.at(-1) || 0) + Math.round(Math.random() * 30 + 10)];
+	}
+
+	function remove() {
+		values.pop();
+		values = values; // Svelte is reactive on assignment only.
+	}
+</script>
+
+<h2>Reactive Example Chart</h2>
+<BarChartReactive {values} />
+
+<h2>D3 Example Chart</h2>
+<BarChartD3 {values} />
+
+<div class="buttons">
+	<button on:click={randomize}>
+		Randomize values
+	</button>
+	<button on:click={add}>
+		Add
+	</button>
+	<button on:click={remove}>
+		Remove
+	</button>
+</div>
+
+<style>
+	.buttons {
+		margin-top: 1em;
+		display: flex;
+		gap: 0.5em;
+	}
+	.buttons > * { flex-grow: 1; }
+</style>
