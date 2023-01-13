f53943b998ea6d473e4e9ec2710d084aed163790
You Sun Song <y.song@campus.lmu.de>
2023-01-13 18:38:54 +0100

    Make timeline represent actual data from store
    Remove mock data for timeline

--- src/lib/Timeline.svelte
+++ src/lib/Timeline.svelte
@@ -1,36 +1,33 @@
 <script>
 	import {
-		timeFormat,
 		line,
 		curveLinear,
 		scaleLinear,
 		extent,
 		bisector,
-		formatLocale,
 		select,
 		axisBottom,
 		axisLeft,
 		scaleUtc,
 		zoom,
-		pointer,
+		pointer, timeFormat, timeParse,
 	} from "d3";
-	import data from "../assets/mock-data-timeline.js";
 	import TooltipPoint from "./TooltipPoint.svelte";
 	import Tooltip from "./Tooltip.svelte";
+	import { data, filter } from "../stores.js";
 
 	let el;
+	let dataGermany;
 	let pinXAxis, pinYAxis, bindLineZoom;
+	let point;
 	let newScale = null;
+	const formatDate = timeFormat("%Y %b");
 
-	// format for tooltip
-	const localFormat = formatLocale({
-		"decimal": ".",
-		"thousands": ",",
-		"grouping": [3],
-		"currency": ["", "€"],
-	});
-	const euro = localFormat.format("$,.2f");
-	const formatTime = timeFormat("%b %d %Y");
+	//data from store
+	$: dataGermany = Object.entries($data).map(([date, states]) => ({
+		date: timeParse("%Y-%m")(date),
+		value: states["de"][$filter.variable],
+	}));
 
 	let width = 1080;
 	const height = 130;
@@ -38,17 +35,17 @@
 
 	// scales
 	$: xScale = scaleUtc()
-		.domain(extent(data, d => d.date))
+		.domain(extent(dataGermany, d => d.date))
 		.range([margin.left, width - margin.right]);
 
 	$: yScale = scaleLinear()
-		.domain(extent(data, (d) => d.revenue)).nice()
+		.domain(extent(dataGermany, d => d.value)).nice()
 		.range([height - margin.bottom, margin.top]);
 
 	const chartLine = (data, xScale) => line()
 		.curve(curveLinear)
 		.x((d) => xScale(d.date))
-		.y((d) => yScale(d.revenue))
+		.y((d) => yScale(d.value))
 		// eslint-disable-next-line no-unexpected-multiline
 		(data);
 
@@ -64,7 +61,7 @@
 			.attr("x", 3)
 			.attr("text-anchor", "start")
 			.attr("font-weight", "bold")
-			.text(data.y));
+			.text(dataGermany.y));
 
 	// draw Axis
 	$: if (pinXAxis) {
@@ -77,18 +74,18 @@
 	// select point for tooltip
 	const bisect = bisector((d) => d.date).center;
 
-	let point = data[0];
+	$: point = dataGermany[0];
 
 	function handleMousemove(event) {
 		const [mx] = pointer(event);
 
 		// returns point to right of our current mouse position
-		let i = bisect(data, xScale.invert(mx));
+		let i = bisect(dataGermany, xScale.invert(mx));
 		if (newScale !== null) {
-			i = bisect(data, newScale.invert(mx));
+			i = bisect(dataGermany, newScale.invert(mx));
 		}
-		if (i < data.length) {
-			point = data[i]; // update point
+		if (i < dataGermany.length) {
+			point = dataGermany[i]; // update point
 		}
 	}
 
@@ -96,17 +93,19 @@
 	const tooltipCoords = {};
 	tooltipCoords.lineLength = height - margin.bottom;
 	// check if scale has changed due to zoom
-	$: if (newScale !== null) {
-		tooltipCoords.x = newScale(point.date);
-		tooltipCoords.y = yScale(point.revenue);
-	} else {
-		tooltipCoords.x = xScale(point.date);
-		tooltipCoords.y = yScale(point.revenue);
+	$: if (point !== undefined) {
+		if (newScale !== null) {
+			tooltipCoords.x = newScale(point.date);
+			tooltipCoords.y = yScale(point.value);
+		} else {
+			tooltipCoords.x = xScale(point.date);
+			tooltipCoords.y = yScale(point.value);
+		}
 	}
 
 	// zoom
 	$: zoomX = zoom()
-		.scaleExtent([1, 32])
+		.scaleExtent([1, 9])
 		.extent([[margin.left, 0], [width - margin.right, height]])
 		.translateExtent([[margin.left, -Infinity], [width - margin.right, Infinity]])
 		.on("zoom", updateChart);
@@ -122,7 +121,7 @@
 		// update X Axis
 		select(pinXAxis).call(xAxis, newScale);
 		// update Line
-		select(bindLineZoom).attr("d", chartLine(data, newScale));
+		select(bindLineZoom).attr("d", chartLine(dataGermany, newScale));
 	}
 </script>
 
@@ -135,7 +134,9 @@
 </style>
 
 <div id="area" bind:this={el} bind:clientWidth={width} transform="translate({margin.left}, {margin.top})">
-	<Tooltip date={formatTime(point.date)} value={euro(point.revenue)} {tooltipCoords} />
+	{#if point !== undefined}
+		<Tooltip date={formatDate(point.date)} value={point.value} {tooltipCoords} />
+	{/if}
 	<svg on:mousemove={handleMousemove}>
 		<!-- clipPath -->
 		<defs>
@@ -154,7 +155,7 @@
 			<path
 				bind:this={bindLineZoom}
 				id="line"
-				d={chartLine(data, xScale)}
+				d={chartLine(dataGermany, xScale)}
 				fill="none"
 				stroke="rebeccapurple"
 				stroke-width="2"
@@ -176,7 +177,7 @@
 			transform="translate(0, {height})"
 		/>
 
-		<!-- Tooltip line and Point -->
+		<!-- Tooltip line and Point onclick={handleMouseClick}/ -->
 		<g class="tooltipPoint" clip-path="url(#clip)">
 			<TooltipPoint {tooltipCoords} />
 		</g>
