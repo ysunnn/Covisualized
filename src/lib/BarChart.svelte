a20bd25d76cb493081dbd0279ccbf18279cd8b0f
JankaDeldok <hartmannja@cip.ifi.lmu.de>
2023-01-19 21:15:22 +0100

    fix barchart and filter data

--- src/lib/BarChart.svelte
+++ src/lib/BarChart.svelte
@@ -2,20 +2,22 @@
 	import { scaleBand, scaleLinear } from "d3-scale";
 
 	export let data;
+	$: console.log(data);
 
-	const width = 800;
+	const width = 600;
 	const height = 400;
 
-	const margin = { top: 10, right: 20, bottom: 20, left: 180 };
+	const margin = { top: 10, right: 20, bottom: 20, left: 30 };
 	const innerHeight = height - margin.top - margin.bottom;
 	const innerWidth = width - margin.left - margin.right;
 
 	$: xDomain = data.map((d) => d.key);
 	$: yDomain = data.map((d) => d.value);
+	$: min = Math.min(...data.map(({ value }) => value));
 
 	$: yScale = scaleBand().domain(xDomain).range([0, innerHeight]).padding(0.1);
 	$: xScale = scaleLinear()
-		.domain([Math.min.apply(null, yDomain), Math.max.apply(null, yDomain)])
+		.domain([Math.min.apply(null, yDomain) - 1.99, Math.max.apply(null, yDomain) + 1.99])
 		.range([0, innerWidth]);
 </script>
 
@@ -38,25 +40,36 @@
 			>
 				{d.key}
 			</text>
-			{#if d.value >= 0}
+			{#if min > 0}
 				<rect
-					x={xScale(0)}
+					x="0"
 					y={yScale(d.key)}
-					width={xScale(d.value) - xScale(0)}
+					width={xScale(d.value)}
 					height={yScale.bandwidth() * 0.8}
 					fill="#0062B1"
 					fill-opacity={Math.abs(xScale(d.value)) / Math.abs(Math.max.apply(null, yDomain))}
 				/>
-			{/if}
-			{#if d.value < 0}
-				<rect
-					x={xScale(d.value)}
-					y={yScale(d.key)}
-					width={xScale(0) - xScale(d.value)}
-					height={yScale.bandwidth() * 0.8}
-					fill="rgb(255,0,0)"
-					fill-opacity={Math.abs(xScale(0) - xScale(d.value)) / Math.abs(Math.min.apply(null, yDomain)) + 0.2}
-				/>
+			{:else}
+				{#if d.value >= 0}
+					<rect
+						x={xScale(0)}
+						y={yScale(d.key)}
+						width={xScale(d.value) - xScale(0)}
+						height={yScale.bandwidth() * 0.8}
+						fill="#0062B1"
+						fill-opacity={Math.abs(xScale(d.value)) / Math.abs(Math.max.apply(null, yDomain))}
+					/>
+				{/if}
+				{#if d.value < 0}
+					<rect
+						x={xScale(d.value)}
+						y={yScale(d.key)}
+						width={xScale(0) - xScale(d.value)}
+						height={yScale.bandwidth() * 0.8}
+						fill="rgb(255,0,0)"
+						fill-opacity={Math.abs(xScale(0) - xScale(d.value)) / Math.abs(Math.min.apply(null, yDomain)) + 0.2}
+					/>
+				{/if}
 			{/if}
 		{/each}
 	</g>
--- src/lib/details/BarChartStates.svelte
+++ src/lib/details/BarChartStates.svelte
@@ -1,10 +1,15 @@
 <script>
-	import { statesForVariableAtDate } from "../../stores";
+	import { statesForVariableAtDate, filter } from "../../stores";
 	import BarChart from "../BarChart.svelte";
 
-	$: data = Object.entries($statesForVariableAtDate.states).map(([key, values]) => {
-		return { key, value: values.value * 100 };
-	});
+	$: data = Object.entries($statesForVariableAtDate.states)
+		.filter(([, values]) => values?.value)
+		.map(([key, values]) => {
+			return {
+				key,
+				value: (values.value - $statesForVariableAtDate.states[$filter.state].value) * 100,
+			};
+		});
 </script>
 
 <BarChart {data} />
