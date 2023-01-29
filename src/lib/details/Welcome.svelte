653849b3301d8517fc3a6a631fe6e6df8b3a3f16
tphan <phantimy1999@gmail.com>
2023-01-29 14:23:00 +0100

    updated welcome page

--- src/lib/details/Welcome.svelte
+++ src/lib/details/Welcome.svelte
@@ -8,12 +8,13 @@
 </p>
 <p>
 	The <strong>map</strong> gives an overview of the currently selected variable
-	({$filter.variable}) <strong>per state</strong>.
+	({$filter.variable}) <strong>per state</strong>. States can be selected for more details by
+	clicking on them or deselected by clicking on them again.
 </p>
 <p>
 	On the <strong>timeline</strong>, the national total can be seen for each day, as well as a
-	state-specific line if a state is selected by clicking on it. A state can be de-selected by
-	clicking on it again.
+	state-specific line if a state is selected. The date is selected with the scrubber and can be
+	moved automatically forward in time by using the play button.
 </p>
 <p>
 	When <strong class="covid">COVID-19 regulations</strong> are active, these are shown in the
