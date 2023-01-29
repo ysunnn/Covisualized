e2299f3ff6c121e94d9c227e23c4429ccfca291e
Felix Bastian <f.bastian@campus.lmu.de>
2023-01-29 22:59:26 +0100

    Calculate min & max values for yearly comparison chart

--- src/lib/details/BarChartStates.svelte
+++ src/lib/details/BarChartStates.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { extent } from "d3";
+	import { isNullish, stateIDs } from "../../util";
 	import { data, filter, statesForVariableAtDate, availableDatesForVariable } from "../../stores";
 	import HorizontalBarChart from "./HorizontalBarChart.svelte";
 	import VerticalBarChart from "./VerticalBarChart.svelte";
@@ -14,14 +15,22 @@
 
 	let currentTab;
 
-	$: minMaxDiff = Math.max(...Object.values($data).map(states => {
+	$: statesMinMaxDiff = Math.max(...Object.values($data).map(states => {
 		return extent(Object.entries(states)
 			.filter(([state]) => state !== "de")
 			.map(([, variables]) => variables[$filter.variable]));
 	}).filter(([min]) => min !== undefined).map(([min, max]) => max - min));
 
-	$: stateChartData = Object.entries($statesForVariableAtDate.states)
-		.filter(([state, values]) => values?.value && state !== $filter.state)
+	$: yearsMinMaxDiff = Math.max(...new Array(12).fill().map((_, i) => String(i + 1).padStart(2, "0"))
+		.flatMap(month => {
+			const yearsForMonth = $availableDatesForVariable.filter(date => date.slice(-2) === month);
+			return stateIDs.map(stateID => {
+				return extent(yearsForMonth.map(date => $data[date][stateID]?.[$filter.variable]));
+			}).filter(([min]) => min !== undefined).map(([min, max]) => max - min);
+		}));
+
+	$: statesChartData = Object.entries($statesForVariableAtDate.states)
+		.filter(([state, values]) => !isNullish(values?.value) && state !== $filter.state)
 		.map(([state, values]) => ({
 			key: state,
 			value: (values.value - $statesForVariableAtDate.states[$filter.state].value),
@@ -29,27 +38,29 @@
 		.sort(({ value: a }, { value: b }) => b - a);
 
 	$: yearsChartData = $availableDatesForVariable
-		.filter(date => date.slice(-2) === $filter.date.slice(-2))
+		.filter(date => !isNullish($data[date][$filter.state]?.[$filter.variable]) && (date !== $filter.date) && (date.slice(-2) === $filter.date.slice(-2)))
 		.map(date => ({
 			key: date,
-			value: $data[$filter.date]?.[$filter.state]?.[$filter.variable] - $data[date]?.[$filter.state]?.[$filter.variable],
+			value: $data[date]?.[$filter.state]?.[$filter.variable] - $data[$filter.date]?.[$filter.state]?.[$filter.variable],
 		}));
-	$: console.log(yearsChartData);
 </script>
 
 <Tabs bind:activeTabValue={currentTab} items={tabItems} />
 {#if 1 === currentTab}
 	<div class="charts">
-		<DetailOverview data={stateChartData} />
+		<h3>{tabItems[currentTab - 1].label}</h3>
+		<DetailOverview data={statesChartData} />
 	</div>
 {/if}
 {#if 2 === currentTab}
 	<div class="charts">
-		<HorizontalBarChart data={stateChartData} min={-minMaxDiff} max={minMaxDiff} />
+		<h3>{tabItems[currentTab - 1].label}</h3>
+		<HorizontalBarChart data={statesChartData} min={-statesMinMaxDiff} max={statesMinMaxDiff} />
 	</div>
 {/if}
 {#if 3 === currentTab}
 	<div class="charts">
-		<VerticalBarChart data={yearsChartData} min={-minMaxDiff} max={minMaxDiff} />
+		<h3>{tabItems[currentTab - 1].label}</h3>
+		<VerticalBarChart data={yearsChartData} min={-yearsMinMaxDiff} max={yearsMinMaxDiff} />
 	</div>
 {/if}
