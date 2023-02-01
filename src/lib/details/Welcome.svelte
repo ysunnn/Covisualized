8289bcdb342562b2ffc39989d3296bd7215d3ebc
tphan <phantimy1999@gmail.com>
2023-02-01 16:31:02 +0100

    compactized text

--- src/lib/details/Welcome.svelte
+++ src/lib/details/Welcome.svelte
@@ -8,21 +8,23 @@
 </p>
 <p>
 	The <strong>map</strong> gives an overview of the currently selected variable
-	({$filter.variable}) <strong>per state</strong>. States can be selected for more details by
-	clicking on them or deselected by clicking on them again. <strong>Note:</strong> the map does
-	not completely represent topographic scale. The city-states Bremen, Berlin and Hamburg have been
-	magnified so that they can be seen and clicked more easily.
+	({$filter.variable}) <strong>per state</strong> per month. Details for a state
+	can be toggled by clicking on them.
 </p>
 <p>
-	On the <strong>timeline</strong>, the national total can be seen for each day, as well as a
-	state-specific line if a state is selected. The date is selected with the scrubber and can be
-	moved automatically forward in time by using the play button.
+	On the <strong>timeline</strong>, the national total can be seen for each month, as well as a
+	state-specific line if a state is selected. A month is selected via playback or scrubber.
 </p>
 <p>
-	When <strong class="covid">COVID-19 regulations</strong> are active, these are shown in the
+	When <strong class="covid">COVID-19 regulations</strong> are in place, these are shown in the
 	bars above the states in the map with <strong class="covid">red coloring</strong>.
 </p>
-
+<p>
+	<small>
+		<strong>Note:</strong> States are not shown to topographic scale. The city-states Bremen,
+		Berlin and Hamburg have been magnified so that they can be seen and clicked more easily.
+	</small>
+</p>
 <style>
 	.covid {
 		color: var(--c-covid);
